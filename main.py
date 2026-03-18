d2ac2b4e59985d512d8bc62c64ce76540b35c3dd
n3navizhy <93191625+n3navizhy@users.noreply.github.com>
2026-03-18 10:01:01 +0300

    add classifier
    Добавил модель deepseek-r1:8b, написал сервис с промптом по классификации текста по трем категориям

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+import asyncio
+import os
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import Command
+from ollama_service import get_ollama_response
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+@dp.message(Command("start"))
+async def process_start_command(message: types.Message):
+    await message.reply("Привет! Напиши мне что-нибудь!")
 
+@dp.message(F.text)  # Обработчик любого текста
+async def echo_message(message: types.Message):
+    await message.reply("🤖 Думаю...")
+    response = get_ollama_response(message.text)
+    await message.reply(response)
+async def main():
+    await dp.start_polling(bot)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    asyncio.run(main())
