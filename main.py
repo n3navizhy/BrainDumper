f130bd00310bfed7daf3683acaeac2ffbf5f2450
n3navizhy <93191625+n3navizhy@users.noreply.github.com>
2026-03-19 10:45:32 +0300

    Add proxy
    Добавил прокси для работы ТГ API, подготовил сервис для дальнейшей работы, далее буду обрабатывать текст в зависимости от класса сообщения

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import asyncio
 import os
+import aiohttp
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, types, F
+from  aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from ollama_service import get_ollama_response
+from aiogram.client.session.aiohttp import AiohttpSession
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
+PROXY_URL = os.getenv('PROXY_URL')
 
-@dp.message(Command("start"))
-async def process_start_command(message: types.Message):
-    await message.reply("Привет! Напиши мне что-нибудь!")
-
-@dp.message(F.text)  # Обработчик любого текста
-async def echo_message(message: types.Message):
-    await message.reply("🤖 Думаю...")
-    response = get_ollama_response(message.text)
-    await message.reply(response)
 async def main():
+
+    session = AiohttpSession(  proxy=PROXY_URL,  # Вот так нужно передавать прокси
+        timeout=60
+    )
+
+    bot = Bot(token=TOKEN, session=session)
+    dp = Dispatcher()
+
+    @dp.message(Command("start"))
+    async def process_start_command(message: types.Message):
+        await message.reply("Привет! Напиши мне что-нибудь!")
+
+    @dp.message(F.text)  # Обработчик любого текста
+    async def echo_message(message: types.Message):
+        await message.reply("🤖 Думаю...")
+        response = await asyncio.to_thread(get_ollama_response, message.text)
+        await message.reply(response)
+
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
